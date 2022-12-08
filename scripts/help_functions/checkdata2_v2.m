c76ff26b7bc17957150e2fa95a40ca9ec681882b
Rosa Gierens <rgierens@uni-koeln.de>
2022-12-08 16:23:30 +0100

    new idea for how to correct backward jump: rewrote entire checkdata2_v2 function

--- scripts/help_functions/checkdata2_v2.m
+++ scripts/help_functions/checkdata2_v2.m
@@ -1,130 +1,282 @@
 function data = checkdata2_v2(data)
-% Sometimes issues with time stamp due to GPS failures. Checking if time
-% goes "backwards" or duplicate time stamps presents
-% RG 21.10.2022     
+% Sometimes issues with time stamp due to GPS failures (presumably). 
+% Checking if time goes "backwards" or duplicate time stamps presents, and 
+% correct time stamps where necessary. Lukas had the great idea of checking 
+% if we can find a corresponding forward jump and correct the time stamp.
+% RG 1.12.2022     
+
+data.timeshift = zeros(size(data.time)); % record how much time shifted 
+% positive value means that the amount was added to the original time
+% stamp, to get back the original timestamp, calculate 
+% timestamp(=time+sampleTms*1e-3) - timeshift
 
 if ~any(diff(double(data.time) + double(data.sampleTms).*1e-3) <= 0 ) 
+    % no problem found
     return
 end
 
 timestamp = double(data.time) + double(data.sampleTms).*1e-3;
 
-%% 1st check: is there an isolated forward jumping time stamp?
-% remove this
-ind = find(diff(timestamp) < 0 );
+% find temporal resolution of measurement: assume most common difference
+% between time stamps is the expected duration of measuring each profile
+time_res = median(diff(timestamp));
 
-for tt = fliplr(ind) % loop from end to the beginning
-    
-    % check if next time stamp smaller AND if next time stamp larger than previous one 
-    if timestamp(tt+1) < timestamp(tt) && timestamp(tt+1) > timestamp(tt-1)
-        % remove all data related to this time stamp
-        timestamp(tt) = [];
-        data = remove_data(data, tt);
+%% correct backward jumps 
+
+% one file (joyrad94_20170522150002_P05_ZEN.lv0) found that has problems 
+% to the extend that the algorithm cannot handle them: creating an
+% exception to deal with this 
+
+if data.time(1) == 517158002 && data.n_levels == 1021 && all(data.range_offsets == [1 76 226 494])
+
+    data = special_case(data, timestamp, 565:625);
+
+% same issue for joyrad94_20170527210000_P05_ZEN.lv0
+elseif data.time(1) == 517611600 && data.n_levels == 1021 && all(data.range_offsets == [1 76 226 494])
+
+    data = special_case(data, timestamp, 225:276);
+
+else 
+
+    % within the function correct_backward_jump, if there are jumps very close
+    % to each other only one of them is evaluated, since experience showed that
+    % they are often connected and it is enough to correct the last one. 
+    % For the rare cases where the problems is not fixed like this, the
+    % function is called in a loop, so that the previously ignored jumps also
+    % get corrected.
+
+    nn = 0;
+    while any(diff(timestamp) < 0 )
+        nn = nn + 1;
+
+        data = correct_backward_jump(data, timestamp, time_res);
+
+        % time most likely edited in function, update time stamp
+        timestamp = double(data.time) + double(data.sampleTms).*1e-3;    
+
+        if nn > 5
+            disp('backward jump in time not resolved')
+            break
+        end
+
     end
-            
-end % tt
-
-if length(data.time) ~= data.totsamp % change in time dimension 
-    data.totsamp = length(data.time);
-    data.totsampchangelabel = 1;
 
 end
 
+
+% check that no data point outside of the hour being processed
+data = checkdata1(data);
+
 % if problem solved, return
-if ~any(diff(double(data.time) + double(data.sampleTms).*1e-3) <= 0 ) 
+if ~any(diff(double(data.time) + double(data.sampleTms).*1e-3) <= 0 )
     return
 end
 
 
-%% 2nd check: remove duplicate time stamps
-
-timestamp = double(data.time) + double(data.sampleTms).*1e-3; % update time stamp
-ind = find(diff(timestamp) == 0 );
-
-% remove second instance of the duplicate time stamp
-ind = ind + 1 ;
-data = remove_data(data, ind);
-
-
-if length(data.time) ~= data.totsamp % change in time dimension 
-    data.totsamp = length(data.time);
-    data.totsampchangelabel = 1;
-
-end
-
-% if problem solved, return
-if ~any(diff(double(data.time) + double(data.sampleTms).*1e-3) <= 0 ) 
-    return
-end
-
-
-%% 3rd check: time jumps backwards
+%% for dublicate time stamp: move the second of the duplicates to be 
+% half way between previous and next one
 
 timestamp = double(data.time) + double(data.sampleTms).*1e-3;
-ind = find(diff(timestamp) < 0 );
+ind = find(diff(timestamp) == 0 )+1; % add +1 to index the second occurrence of the duplicate
 
-% in case last data point is affected
-if any(ind ==  length(timestamp)-1) % ind indicates last "okay" time stamp
-    data = remove_data(data, length(timestamp) );
-    ind( ind == length(timestamp)-1) = []; % remove from ind list
-    timestamp(end) = [];
-end % ind
+for tt = ind
 
+    if tt == length(data.time) % duplicate time stamp at the end of the file
+        add_time = time_res;
+        
+    else
+        % calculate mean of duplicate and next time stamp after dubplicate stamp
+        add_time = (timestamp(tt+1) - timestamp(tt-1)) / 2;
+        add_time = min(add_time, time_res); % don't add more than the temporal resolution
+    end
+    
+    new_time = timestamp(tt) + add_time;
+    
+    data.time(tt) = floor(new_time);
+    data.sampleTms(tt) = (new_time - floor(new_time)).*1e3;
 
-for tt = fliplr(ind)
-
-    % find next time stamp that is after the "last okay time stamp"
-    ixnxt = find(timestamp(tt) < timestamp(tt+1:end),1);
-
-    data = remove_data(data, (tt+1):(tt+ixnxt-1) );
-   
-end % tt 
-
-if length(data.time) ~= data.totsamp % change in time dimension 
-    data.totsamp = length(data.time);
-    data.totsampchangelabel = 1;
+    data.timeshift(tt) = data.timeshift(tt) + add_time;
+        
+    data.totsampchangelabel = 1; % note for later that issues with time found
 
 end
 
-% if problem solved, return
+
+% check that no data point outside of the hour being processed
+% probably not necessary here, but better safe than sorry
+data = checkdata1(data);
+
+
 if ~any(diff(double(data.time) + double(data.sampleTms).*1e-3) <= 0 ) 
     return
 end
+
 
 %% still a problem with time? 
-
+    
 disp('WARNING ! This not programmed yet')
 
 end % function
 
 
-
-function data = remove_data(data, ix)
-
-    dim_t = length(data.time);
-    
-    % loop for all variables
-    fields = fieldnames(data);
-
-    for ff = 1:length(fields)
-
-        vardim = size(data.(fields{ff}));
-
-        if ~any(vardim == dim_t)
-            continue
-        end
+function data = correct_backward_jump(data, timestamp, time_res)
 
 
-        if prod(vardim) == dim_t % 1 dim variables
-            data.(fields{ff})(ix) = [];
+    % find backward jumps
+    ind = find(diff(timestamp) < 0 );
 
-        elseif length(vardim) == 2 % 2 dim variables
-            data.(fields{ff})(ix,:) = [];
-
-        elseif length(vardim) == 3 % 3 dim variables
-            data.(fields{ff})(ix,:,:) = [];
-
-        end
+    % in a couple of files two jumps occur close to each other, which messes up
+    % with the algorithm. solution in the cases found was to ignore first jump
+    if any(diff(ind) < 5)
+        ind( diff(ind) < 5 ) = [];
     end
-    
+
+    for tt = ind
+
+        figure, plot(timestamp, '.'), xlim([tt-20, tt+10]), hold on
+
+        % 1. check if backward jump associated with a corresponding forward jump before
+
+        % starting from the time stamp after the backward jump, calculate
+        % backwards what the time stamps would be base corresponding to the
+        % temporal resolution, and compare to the logged timestamp
+
+        testtime = timestamp(tt+1) - [1:tt].*time_res;
+
+        %  where time stamp within +- 0.3 sec from testtime
+        ix_close = abs(testtime - flip(timestamp(1:tt))) < 0.3;    
+        ix = find(ix_close, 1); % find first ocurrence
+
+        % found possible match going backwards:
+        % criteria 1: a match found in the past 
+        % criteria 2: at the point when timestamps start to align, is there a forward jump of at least time_res+0.3
+        %   second criteria is to avoid drifting towards similar time due to
+        %   inprecision of the time resolution
+        %
+        % -> consider that found the corresponding forward jump -
+
+        if ~isempty(ix) && (timestamp(tt-ix+2)-timestamp(tt-ix+1)) > time_res+0.3
+
+            new_time =  flip(testtime(1:ix-1)); % time to be filled in
+
+            ix_ed = (tt-ix+2:tt); % index of original time array to be edited
+
+            data.timeshift(ix_ed) =  data.timeshift(ix_ed) + ... % calculate cumulatively, since possible that same time stamp gets modified more than once
+                new_time - ( double(data.time(ix_ed)) + double(data.sampleTms(ix_ed)).*1e-3 );
+
+            data.time(ix_ed) = floor(new_time);
+            data.sampleTms(ix_ed) = (new_time-floor(new_time) ) .*1e3;
+
+            data.totsampchangelabel = 1; % note for later that issues with time found
+
+        % 2. if no corresponding forward jump before, correct forward
+        else 
+
+            %  where time stamp within +- 0.3 sec from testtime
+            testtime = timestamp(tt) + ([tt : length(data.time)-1] -tt+1).*time_res;
+
+            ix_close = abs(testtime - timestamp(tt+1:end)) < 0.3;
+            ix = find(ix_close, 1); % find first ocurrence
+
+            % found possible match going forwards
+            % as above, also check that a associated with a jump
+            if any(ix_close) && (timestamp(tt+ix)-timestamp(tt+ix-1)) > time_res+0.3
+
+                new_time = testtime( 1: ix-1); % time to be filled in
+
+                ix_ed = ( tt+1: tt+ix-1); % index of original time array to be edited
+
+                data.timeshift(ix_ed) =  data.timeshift(ix_ed) + ... % calculate cumulatively, since possible that same time stamp gets modified more than once
+                    new_time - ( double(data.time(ix_ed)) + double(data.sampleTms(ix_ed)).*1e-3 );
+
+                data.time(ix_ed) = floor(new_time);
+                data.sampleTms(ix_ed) = (new_time-floor(new_time) ) .*1e3;
+
+                data.totsampchangelabel = 1; % note for later that issues with time found
+
+
+
+            else % no matching time found afterwards (and also not before, because that checked above)
+                tix = tt; 
+
+                % now it is of course possible that now the backwards jump has
+                % moved
+
+                % continue until end of file or until a forward jump occurs 
+                % (could be because of the internal calibration)
+                while (timestamp(tix+1) - timestamp(tix)) < 0 
+
+                    % shift time stamp to be the time stamp before jump + time res
+                    new_time = timestamp(tix) + time_res;
+
+                    data.timeshift(tix+1) = data.timeshift(tix+1) + new_time - ...
+                         ( double(data.time(tix+1)) + double(data.sampleTms(tix+1)).*1e-3 );
+
+
+                    % need to modify also variable timestamp, as this used in
+                    % the statement of the while loop
+                    data.time(tix+1) = floor(new_time);
+                    data.sampleTms(tix+1) = (new_time-floor(new_time) ) .*1e3;
+                    timestamp(tix+1) =  new_time;
+
+                    data.totsampchangelabel = 1; % note for later that issues with time found
+
+                    tix = tix+1; % move to check next time stamp
+                    
+                    if tix == length(data.time) % reached end of file
+                        break
+                    end
+                end
+
+            end
+
+        end % fi
+
+        flag_edittime = data.timeshift ~= 0 ;
+
+        plot(find(flag_edittime),  double(data.time(flag_edittime)) + double(data.sampleTms(flag_edittime)).*1e-3 , 'or')
+        legend('original timestamp', 'modified time stamp', 'location', 'northwest')
+        xlim([tt-20, find(flag_edittime, 1, 'last')+10])
+
+        this_date = datestr(double(data.time(1))/3600/24 + datenum([2001,1,1,0,0,0]), 'yyyymmddHH');
+
+        title(this_date)
+        savefig(['timetstamp_shiftcheck_' this_date '_' num2str(tt)])
+        close
+
+    end % tt
+
+
 end % function
+
+
+function data = special_case(data, timestamp, ind_out)
+
+    ind = false(size(timestamp));
+    ind(ind_out) = 1;
+
+    figure, plot(timestamp, '.'), xlim([find(ind,1)-20, find(ind,1,'last')+20]), hold on
+
+    new_time = interp1(find(~ind), timestamp(~ind), find(ind), 'linear');
+    
+    data.timeshift(ind) =  data.timeshift(ind) + ... % calculate cumulatively, since possible that same time stamp gets modified more than once
+        new_time - ( double(data.time(ind)) + double(data.sampleTms(ind)).*1e-3 );
+
+    data.time(ind) = floor(new_time);
+    data.sampleTms(ind) = (new_time-floor(new_time) ) .*1e3;
+
+    data.totsampchangelabel = 1; % note for later that issues with time found
+
+    
+    flag_edittime = data.timeshift ~= 0 ;
+
+    plot(find(flag_edittime),  double(data.time(flag_edittime)) + double(data.sampleTms(flag_edittime)).*1e-3 , 'or')
+    legend('original timestamp', 'modified time stamp', 'location', 'northwest')
+        
+    this_date = datestr(double(data.time(1))/3600/24 + datenum([2001,1,1,0,0,0]), 'yyyymmddHH');
+
+    title(this_date)
+    savefig(['timetstamp_shiftcheck_' this_date '_specialcase'])
+    close
+
+end
